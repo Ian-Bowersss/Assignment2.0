05c4d08188d6ec7b5f6cae529a9fc1c1bf5ea2a3
Ian-Bowersss <156280899+Ian-Bowersss@users.noreply.github.com>
2024-04-25 22:26:43 -1000

    Add files via upload

---
+++ public/store.js
@@ -0,0 +1,96 @@
+
+
+// get the query string into a easy to use object
+const params = (new URL(document.location)).searchParams;
+let errors = {};
+let quantities = [];
+
+// check if the query string has errors, if so parse it
+if (params.has('errors')) {
+  errors = JSON.parse(params.get('errors'));
+  // get the quantities also to insert into the form to make sticky
+  quantities = JSON.parse(params.get('quantities'));
+
+}
+
+// IR4 
+function newPurchaseButton() {
+  //declares variable to change the value of the Purchase button
+  const purchaseButton = document.getElementById("PurchaseButton");
+  // makes sure length of array is greater than 0 and makes sure array is present and that all elements are = 0
+  if (quantities.length > 0 && quantities.every(qty => parseInt(qty) == 0)) {
+    purchaseButton.value = "Please Select Some Items to Purchase";
+    //if error occurs of a nonNegInt it will display this message inside the purchase button
+  } else if (Object.keys(errors).length > 0) {
+    purchaseButton.value = "Please fix the errors and try again";
+  } else {
+    purchaseButton.value = "Purchase!";
+  }
+}
+
+let products;
+window.onload = async function () {
+  // use fetch to retrieve product data from the server
+  // once the products have been successfully loaded and formatted as a JSON object
+  // display the products on the page
+  await fetch('products.json').then(await function (response) {
+    if (response.ok) {
+      response.json().then(function (json) {
+        products = json;
+        display_products();
+        newPurchaseButton();
+      });
+    } else {
+      console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
+    }
+  });
+}
+
+// function to perform the filtering of the products
+function myFunction() {
+  var input, filter, ul, si, a, i, txtValue;
+  input = document.getElementById("search_textbox");
+  filter = input.value.toUpperCase();
+  si = document.getElementsByTagName("section");
+  for (i = 0; i < si.length; i++) {
+    a = si[i].getElementsByTagName("h2")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      si[i].style.display = "";
+    } else {
+      si[i].style.display = "none";
+    }
+  }
+}
+
+
+function display_products() {
+  // loop through the products array and display each product as a section element
+  for (let i = 0; i < products.length; i++) {
+    let quantity_label = 'Quantity';
+    // if there is an error with this quantity, put it in the label to display it
+    if ((typeof errors['quantity' + i]) != 'undefined') {
+      quantity_label = `<font class="error_message">${errors['quantity' + i]}</font>`;
+    }
+    let quantity = 0;
+    // put previous quantity in textbox if it exists
+    if ((typeof quantities[i]) != 'undefined') {
+      quantity = quantities[i];
+    }
+    products_main_display.innerHTML += `
+    <section class="item">
+    <div style="text-align: center;">
+    <h2 style=" ;">${products[i].name}</h2>
+        <img src="./images/${products[i].image}" height="150px" width="150px"> 
+    </div>
+    <p>${products[i].quantityAvalible} shoes in stock</p>
+    <p style="font-size: larger; font-weight: bold; text-align: center;">$${products[i].price}</p>
+   
+    <label>${quantity_label}</label>
+   
+    <input type="text" placeholder="0" name="quantity_textbox[${i}]" value="${quantity}">
+
+    </section>
+`;
+  }
+}
---
+++ server.js
@@ -0,0 +1,138 @@
+// loads the products array from the .json file
+const products = require(__dirname + '/products.json');
+const express = require('express');
+const app = express();
+
+//2const usersRegData = require(__dirname + '/user_data.json');
+const fs = require('fs');
+
+let userDataFile = __dirname + '/user_data.json';
+
+//check is user data file exist
+let usersRegData = {};
+if(fs.existsSync(userDataFile)){
+  const data = fs.readFileSync(userDataFile, 'utf-8');
+  usersRegData = JSON.parse(data);
+  let stats = fs.statSync(userDataFile)
+  console.log(`${userDataFile} has ${stats.size} characters stats.size`)
+}
+console.log(usersRegData)
+
+// form data in a POST request 
+const myParser = require("body-parser");
+app.use(myParser.urlencoded({ extended: true }));
+
+// Log requests to the console
+app.all('*', function (request, response, next) {
+  console.log(request.method + ' to ' + request.path);
+  next();
+});
+
+
+app.post('/processLogin', function (request, response) {
+  // Process login form POST and redirect to logged in page if ok, back to login page if not
+  res.json(request.body);
+});
+
+
+// javascript to define products array
+app.get('/products.json', function (req, res, next) {
+  res.json(products);
+});
+
+
+// if quantities are valid, otherwise redirect back to products
+app.post('/process_purchase_form', function (req, res, next) {
+  console.log(req.body)
+  // process purchase form submitted
+  const errors = {}; // assume no errors
+  let quantities = [];
+  if (typeof req.body['quantity_textbox'] != 'undefined') {
+    quantities = req.body['quantity_textbox'];
+    // Loop through the quantities submitted
+    for (let i in quantities) {
+      // validate the quantity inputted is a non-negative integer
+      if (!isNonNegInt(quantities[i])) {
+        errors['quantity' + i] = isNonNegInt(quantities[i], true).join('<br>');
+      } else {
+        const productsIdent = parseInt(i);
+        const quantityReq = parseInt(quantities[i]);
+        //validates that the quantity is less than or equal to the quantity available 
+        if (!isNaN(productsIdent) && products[productsIdent] && products[productsIdent].quantityAvalible < quantityReq) {
+          errors['quantity' + i] = 'Not enough available in inventory';
+        }
+      }
+
+    }
+
+    // Checks if the quantities array has at least one value greater than 0
+    if (!quantities.some(qty => parseInt(qty) > 0)) {
+      errors['quantity'] = 'Please select at least one item to purchase';
+    }
+
+    //Logs the purchase data to the console and where it came from. It is not required!!!
+    console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.body));
+  }
+
+  // create a query string with data from the form
+  const params = new URLSearchParams();
+  params.append('quantities', JSON.stringify(quantities));
+  // If there are errors, send user back to fix otherwise redirect to invoice with the quantities in the query string
+
+  // If there are errors, send user back to fix otherwise send to invoice
+  if (Object.keys(errors).length > 0) {
+    // errors present , redirect back to store - fix and try again
+    params.append('errors', JSON.stringify(errors));
+    res.redirect('store.html?' + params.toString());
+  } else {
+    upInventory(quantities)
+    //redirects to invoice
+    res.redirect('./invoice.html?' + params.toString());
+    //remove quantities from products.aval
+    for (let i in products) {
+      products[i].quantityAvalible -= request.body[`quantity_textbox${i}`];
+    }
+  }
+
+});
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`));
+
+// updates inventory
+function upInventory(quantities) {
+  for (let i in quantities) {
+    const qty = parseInt(quantities[i]);
+    if (!isNaN(qty) && qty > 0) {
+      const productsIdent = parseInt(i);
+      if (!isNaN(productsIdent) && products[productsIdent] && products[productsIdent].quantityAvalible >= qty) {
+        products[productsIdent].quantityAvalible -= qty;
+        // IR1 count the total quantity sold for the chosen product
+        if (!products[productsIdent].total_sold) {
+          products[productsIdent].total_sold = qty;
+        } else {
+          products[productsIdent].total_sold += qty;
+        }
+      }
+    }
+  }
+}
+
+function isNonNegInt(q, returnErrors = false) {
+  errors = []; // assume no errors at first
+  if (q == '') q = 0; // handle blank inputs as if they are 0
+  if (Number(q) != q) errors.push('Not a number!'); // number value
+  else {
+    if (q < 0) errors.push('Negative value!'); //  non-negative
+    if (parseInt(q) != q) errors.push('Not an integer!'); // integer
+  }
+  return returnErrors ? errors : (errors.length == 0);
+}
+
+
+
+
+
+
+
+
